38443921e23e64461322874a2fcf3833acfcbfb6
Nivedha <krishnannivedha7@gmail.com>
2026-01-10 18:42:30 +0530

    feat: implemented Page Object Model with BaseTest and DriverFactory

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,24 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import utils.DriverFactory;
+
+public class BaseTest {
+	
+	protected WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+		driver = DriverFactory.getDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		DriverFactory.quitDriver();
+	}
+}
