24e61478f5ae352ed25005b95ad8e962ac82a0ad
Nivedha <krishnannivedha7@gmail.com>
2026-01-12 12:45:37 +0530

    feat: added explicit waits to stabilize login tests

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,6 +3,8 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import utils.WaitUtils;
+
 public class LoginPage {
 
 	private WebDriver driver;
@@ -10,20 +12,25 @@ public class LoginPage {
 	private By usernameField = By.id("user-name");
 	private By passwordField = By.id("password");
 	private By loginButton = By.id("login-button");
+	private WaitUtils waitUtils;
 	
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		this.waitUtils = new WaitUtils(driver);
 	}
 	
 	public void enterUsername(String username) {
+		waitUtils.waitForVisibility(usernameField);
 		driver.findElement(usernameField).sendKeys(username);
 	}
 	
 	public void enterPassword(String password) {
+		waitUtils.waitForVisibility(passwordField);
 		driver.findElement(passwordField).sendKeys(password);
 	}
 	
 	public void clickLogin() {
+		waitUtils.waitForClickable(loginButton);
 		driver.findElement(loginButton).click();
 	}
 	
@@ -34,14 +41,18 @@ public class LoginPage {
 		clickLogin();
 	}
 	
+	
+	
 	private By errorMessage = By.cssSelector("[data-test='error']");
 	private By productsTitle = By.className("title");
 	
 	public boolean isErrorMessageDisplayed() {
+		waitUtils.waitForVisibility(errorMessage);
 		return driver.findElement(errorMessage).isDisplayed();
 	}
 	
 	public boolean isLoginSuccessful() {
+		waitUtils.waitForVisibility(productsTitle);
 		return driver.findElement(productsTitle).isDisplayed();
 	}
 	
