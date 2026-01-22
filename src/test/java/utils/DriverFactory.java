602de06a9a8b4b199c05f9b9ebdf6ded33d17ae8
Nivedha <krishnannivedha7@gmail.com>
2026-01-22 17:30:53 +0530

    fix: stabilize Chrome startup for GitHub Actions CI

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -2,29 +2,41 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class DriverFactory {
-private static WebDriver driver;
 
-public static WebDriver getDriver() {
-	if (driver == null) {
-		 String browser = ConfigReader.getProperty("browser");
+    private static WebDriver driver;
 
-	        if (browser.equalsIgnoreCase("chrome")) {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-	}
-	}
-	return driver;
-}
+    public static WebDriver getDriver() {
 
-public static void quitDriver() {
-	if (driver != null) {
-		driver.quit();
-		driver = null;
-	}
-}
+        if (driver == null) {
+
+            String browser = ConfigReader.getProperty("browser");
+
+            if (browser.equalsIgnoreCase("chrome")) {
+
+                ChromeOptions options = new ChromeOptions();
+
+                // REQUIRED for GitHub Actions (Linux)
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver(options);
+            }
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
